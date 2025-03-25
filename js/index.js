d4da9db573e901006ec12dabbb91057bc7af9652
luz <luz098133@gmail.com>
2025-03-25 16:16:54 -0500

    octavo cargo

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function Loguear() {
     let username = document.getElementById('username').value
     let password = document.getElementById('clave').value
 
-    if(username == 'carlitos ' && password == '123456'){
+    if(username == 'carlitos ' && password == '1245'){
         alert('Usuario valido')
     }else{
         alert('usuario incorrecto')
