2ee8b5e0a85a3791833f0e9145fc4402f1fc7e04
luz <luz098133@gmail.com>
2025-03-25 16:30:58 -0500

    noveno cargo

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function Loguear() {
     let username = document.getElementById('username').value
     let password = document.getElementById('clave').value
 
-    if(username == 'carlitos ' && password == '1245'){
+    if(username == 'carlitos ' && password == '123456'){
         alert('Usuario valido')
     }else{
         alert('usuario incorrecto')
