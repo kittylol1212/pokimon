69bb283a411ff6fbf109d9df5a1f99a2bb3407f6
luz <luz098133@gmail.com>
2025-03-25 15:50:29 -0500

    cuarto cargo

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function Loguear() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('clave').value
+
+    if(username == 'Rodolfo' && password == '123456'){
+        alert('Usuario valido')
+    }else{
+        alert('usuario incorrecto')
+    }  
+
+}
